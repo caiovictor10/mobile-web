1625da26c64e8b0895336ffafbfab57036d01a5b
caiovictor874@gmail.com <caiovictor874@gmail.com>
2025-09-02 14:28:52 -0300

    terçou

---
+++ Site-front/texto.js
@@ -0,0 +1,17 @@
+let text = "Estão chegando as provas"
+
+// Tamanho da estring
+console.log(text.length);
+
+// Replace
+console.log(text.replace("provas", "avaliações"));
+
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+
+// Retorna a primeira posição na string
+console.log(text.indexOf("as"));
+
+// Retorna a ultima posição na string
+console.log(text.lastIndexOf("as"));
+
