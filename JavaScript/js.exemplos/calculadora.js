3d7c6f2da81bbba3cc40b1098976bc88c3cd9b58
caiovictor874@gmail.com <caiovictor874@gmail.com>
2025-09-04 15:47:46 -0300

    Caio

--- JavaScript/js.exemplos/calculadora.js
+++ JavaScript/js.exemplos/calculadora.js
@@ -1,4 +1,12 @@
 // Variável
 const num1 = document.getElementById("numero1");
 const num2 = document.getElementById("numero2");
+const Resultado = document.getElementById("resultado");
+
+// Função
+function somar(event){
+    event.preventDefault();
+    console.log(num1.value);
+    console.log(num2.value);
+    }
 
