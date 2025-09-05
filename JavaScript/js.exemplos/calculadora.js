4102cd1bc3b91315fdc66e36556eba356272822f
caiovictor874@gmail.com <caiovictor874@gmail.com>
2025-09-05 15:24:19 -0300

    sextou

--- JavaScript/js.exemplos/calculadora.js
+++ JavaScript/js.exemplos/calculadora.js
@@ -4,27 +4,45 @@ const num2 = document.getElementById("numero2");
 const Resultado = document.getElementById("resultado");
 
 // Função
-function somar(event){
-    event.preventDefault();
-    Resultado.innerHTML = Number(num1.value) + Number(num2.value);
-    num1.value = "";
-    num2.value = "";
-    }
-    function subtrair(event){
-    event.preventDefault();
-    Resultado.innerHTML = Number(num1.value) - Number(num2.value);
-    num1.value = "";
-    num2.value = "";
-    }
- function multiplicar(event){
-    event.preventDefault();
-    Resultado.innerHTML = Number(num1.value) * Number(num2.value);
-    num1.value = "";
-    num2.value = "";
- }
-  function dividir(event){
-    event.preventDefault();
-    Resultado.innerHTML = Number(num1.value) / Number(num2.value);
-    num1.value = "";
-    num2.value = "";
-  }
+function somar(event) {
+   event.preventDefault();
+   // Validação de formulário
+   if (num1.value == "" || num2.value == "");
+   alert("Digite algo no campo 1 e 2");
+   return false;
+}
+
+Resultado.innerHTML = Number(num1.value) + Number(num2.value); {
+   num1.value = "";
+   num2.value = "";
+}
+function subtrair(event) {
+   event.preventDefault();
+      // Validação de formulário
+   if (num1.value == "" || num2.value == "");
+   alert("Digite algo no campo 1 e 2");
+   return false;}
+   Resultado.innerHTML = Number(num1.value) - Number(num2.value);{
+   num1.value = "";
+   num2.value = "";
+}
+function multiplicar(event) {
+   event.preventDefault();
+      // Validação de formulário
+   if (num1.value == "" || num2.value == "");
+   alert("Digite algo no campo 1 e 2");
+   return false;}
+   Resultado.innerHTML = Number(num1.value) * Number(num2.value);{
+   num1.value = "";
+   num2.value = "";
+}
+function dividir(event) {
+   event.preventDefault();
+      // Validação de formulário
+   if (num1.value == "" || num2.value == "");
+   alert("Digite algo no campo 1 e 2");
+   return false;}
+   Resultado.innerHTML = Number(num1.value) / Number(num2.value);{
+   num1.value = "";
+   num2.value = "";
+}
