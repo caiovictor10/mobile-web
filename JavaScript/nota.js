35c3fb4cd6d3908bb9056725d4ffa3fefa9c1966
caiovictor874@gmail.com <caiovictor874@gmail.com>
2025-09-10 16:16:07 -0300

    Fechou

--- JavaScript/nota.js
+++ JavaScript/nota.js
@@ -3,6 +3,21 @@ const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("result");
 
 // Função
-function resultadoNotaClick() {
-    alert("Resultado");
+function resultadoNotaClick(event) {
+    event.preventDefault();
+    console.log(inputNota.value);
+
+    //Lógica-condição
+
+    if (inputNota.value >= 6) {
+        //alert ("Aprovado");
+        resultado.innerHTML = "Aprovado";
+    }
+    else {
+        resultado.innerHTML = "Reprovado";
+    }
 }
+
+
+
+
