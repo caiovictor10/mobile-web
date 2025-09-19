3bd6046398f82ae33319639743c17daaeba4a220
caiovictor874@gmail.com <caiovictor874@gmail.com>
2025-09-19 16:22:43 -0300

    foi

--- projeto-b2/src/App.jsx
+++ projeto-b2/src/App.jsx
@@ -1,16 +1,12 @@
+import Contador from "./componentes/Contador"
+
 function App() {
 
 
   return (
    <>
-<h1>Olá Rodriguinho</h1>
-<h2>Giovanni</h2>
-<h3>Senai</h3>
-<h4>Cotia</h4>  
-<hr/>
-
-
-
+   <Contador/>
+   
   </>
   )
 }
