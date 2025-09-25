89fdbf76af55507664d04e4f3bc37db67cc541cd
caiovictor874@gmail.com <caiovictor874@gmail.com>
2025-09-25 15:13:28 -0300

    quinta

--- projeto-b2/src/App.jsx
+++ projeto-b2/src/App.jsx
@@ -1,11 +1,21 @@
-import "./App.css"
+import Banner from "./componentes/Contador/Banner"
+import ImgCard from "./componentes/imgCard"
 import Header from "./Header"
+import "./App.css";
 
 function App() {
     return (
         <main className="container">
 
             <Header title="Meu site"/>
+
+            <Banner>
+                <h1>Bem vindo ao meu site</h1>
+                <p>Aqui você encontra as melhores ofertas</p>
+
+            </Banner>
+
+            <ImgCard />
 
         </main>
     )
---
+++ projeto-b2/src/componentes/imgCard/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ImgCard() {
+  return (
+    <div>olá Img - Popeto</div>
+  )
+}
