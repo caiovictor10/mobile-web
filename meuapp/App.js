b778831840e3a962350692a16ea7a809d9f7d188
caiovictor874@gmail.com <caiovictor874@gmail.com>
2025-09-30 15:11:54 -0300

    meu App

---
+++ meuapp/App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View >
+      <Text>teste popeto</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  width: "100%",
+  heigth: 150, 
+  backgroundColor: "blue" 
+});
