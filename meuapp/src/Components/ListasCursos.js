7ecb614f716efb1e64b5f3e2f6fdf05de898318b
caiovictor874@gmail.com <caiovictor874@gmail.com>
2025-10-01 16:19:27 -0300

    Foi

--- meuapp/src/Components/ListasCursos.js
+++ meuapp/src/Components/ListasCursos.js
@@ -1,18 +1,47 @@
 import { Text, View, StyleSheet, FlatList } from 'react-native'
 
 export default function ListasCursos() {
+    const data = ["React Native", "Mongo DB", "Express", "Node.js", "C#"];
+
   return (
   <View style={estilos.container}>
-<FlatList />
-      data={["React Native", "Node.js", "MongoDB", "HTML Popeto"]}
-      renderItem={({item}) => <Text>•{item}</Text>}
+<FlatList 
+      data={data}
+      renderItem={({item}) => (
+        <View style={estilos.lastItem}>
+      <Text style={estilos.bullet}>• </Text>
+      <Text style={estilos.text}>{item}</Text>
+      
+      </View>
+)}
+  />
   </View>
   )
+
 }
 
 const estilos = StyleSheet.create({
     container: {
         marginTop: 40,
         padding: 20,
+    },
+    lastItem: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 8,
+        padding: 12,
+        backgroundColor: "#ff00aa",
+        borderWidth: 3,
+        borderColor: "bluelight",
+        borderRadius: 20,
+        elevation: 10,
+    },
+    bullet: {
+        fontSize: 15,
+        marginRight: 6,
+    
+    },
+    text: {
+        fontSize: 16,
     }
 })
