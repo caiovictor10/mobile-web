fac454ee697108322f43982f962eb7cc63ba990f
caiovictor10 <caiovictor874@gmail.com>
2025-10-22 15:48:27 -0300

    commit

--- meuapp/src/Telas/Cadastro.js
+++ meuapp/src/Telas/Cadastro.js
@@ -1,46 +1,57 @@
 import { useState } from 'react'
 import { Button, Text, View, TextInput, StyleSheet, Alert } from 'react-native'
-import axios  from 'axios';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Cadastro() {
-    const [nome, setNome]= useState('');
-    const [telefone, setTelefone]= useState('');
+    const [nome, setNome] = useState('');
+    const [telefone, setTelefone] = useState('');
 
-    const enviarContato = async() => {
-        if (!nome || !telefone){
+    const enviarContato = async () => {
+        if (!nome || !telefone) {
             Alert.alert("Erro, por favor, preecha todos os campos");
             return;
         }
 
-        const novoContato = {nome, telefone};
-        axios.post('http://localhost:3000/contatos', novoContato)
-    }
-  return (
-    <View style={estilos.container}>
-        <Text style={estilos.label}>Nome:</Text>
-        <TextInput
-        style={estilos.Input}
-        value={nome}
-        onChange={setNome}
-         placeholder="Digite o nome"
-         />
-         <Text style={estilos.label}>Telefone:</Text>
-        <TextInput
-        style={estilos.Input}
-        value={telefone}
-        onChange={setTelefone}
-         placeholder="Digite o telefone"
-         />
+      const novoContato = { nome, telefone };
 
-         <Button title="Cadastrar" onPress={enviarContato} />
-    </View>
-  )
+    axios.post('http://10.0.2.2:3000/contatos', novoContato)
+      .then(resposta => {
+        if (resposta.status === 201) {
+          setNome('');
+          setTelefone('');
+          navigation.navigate("ListaContatos");
+        } else {
+          Alert.alert("Erro, falha ao adicionar contato.");
+        }
+      });
+    }
+    return (
+        <View style={estilos.container}>
+            <Text style={estilos.label}>😀 Nome:</Text>
+            <TextInput
+                style={estilos.Input}
+                value={nome}
+                onChangeText={setNome}
+                placeholder="Digite o nome"
+            />
+            <Text style={estilos.label}>📞 Telefone:</Text>
+            <TextInput
+                style={estilos.Input}
+                value={telefone}
+                onChangeText={setTelefone}
+                placeholder="Digite o telefone"
+            />
+
+            <Button title="Cadastrar" onPress={enviarContato} />
+        </View>
+    )
 }
 const estilos = StyleSheet.create({
     container: {
         padding: 20,
     },
-    label:{
+    label: {
         fontSize: 18,
         marginBottom: 5,
         fontWeight: "bold"
